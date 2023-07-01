e0dc2fd2f32fccde2731cc066a64f84d060a5964
ShahuPatil07 <135165337+ShahuPatil07@users.noreply.github.com>
2023-07-01 23:05:00 +0530

    Updated moving function with next_turn

--- main.py
+++ main.py
@@ -36,24 +36,30 @@ class Main:
 
                 # click
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    dragger.update_pos(event.pos)
-
-                    clicked_row = dragger.mouse_y // SQSIZE
-                    clicked_col = dragger.mouse_x // SQSIZE
-
-                    # if clicked square has a piece ?
-                    if board.squares[clicked_row][clicked_col].has_piece():
+                        dragger.update_pos(event.pos)
+                   
+                        clicked_row = dragger.mouse_y // SQSIZE
+                        clicked_col = dragger.mouse_x // SQSIZE
+                        if board.squares[clicked_row][clicked_col].has_piece():
                             piece = board.squares[clicked_row][clicked_col].piece
-                        # valid piece (color) ?
-                       
-                            board.possible_moves(piece, clicked_row, clicked_col)
-                            dragger.save_initial_pos(event.pos)
-                            dragger.drag_piece(piece)
+                            if game.next_player==piece.color:
+                                board.possible_moves(piece, clicked_row, clicked_col)
+                                dragger.save_initial_pos(event.pos)
+                                dragger.drag_piece(piece)
                             # show methods 
-                            game.bg(screen)
+                                game.bg(screen)
                             
-                            game.show_moves(screen)
-                            game.add_pieces(screen)
+                                game.show_moves(screen)
+                                game.add_pieces(screen)
+                       
+                       
+                            
+
+                    
+                    
+
+
+                    
                 
                 # mouse motion
                 elif event.type == pygame.MOUSEMOTION:
@@ -87,6 +93,13 @@ class Main:
                         move = Move(initial, final)
 
                         # valid move ?
+                        if board.is_valid_(dragger.piece,move):
+                            board.move(dragger.piece,move)
+                            self.game.bg(self.screen)
+                            self.game.add_pieces(self.screen)
+                            self.game.next_turn()
+            
+
                        
                     
                     dragger.undrag_piece()
