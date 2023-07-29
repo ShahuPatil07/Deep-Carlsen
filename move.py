a4b542d31f8142a4d1baa0ee2ad854e6515eee8a
ShahuPatil07 <135165337+ShahuPatil07@users.noreply.github.com>
2023-07-29 10:59:22 +0530

    Delete move.py

--- move.py
+++
@@ -1,8 +0,0 @@
-class Move:
-    def __init__(self,initial,final):
-        self.initial=initial
-        self.final=final
-    def __eq__(self,move2):
-        if self.initial==move2.initial and self.final==move2.final:
-            return True
-        return False
